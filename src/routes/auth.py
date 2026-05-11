6fafddad5a18da8de0bc94ad3411c2cc46c66c4f
Dishonored1402 <148948081+Dishonored1402@users.noreply.github.com>
2026-05-11 12:19:59 +0300

    1

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -2,6 +2,9 @@ import os
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File, Request, BackgroundTasks
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+import cloudinary
+import cloudinary.uploader
+
 from src.database.db import get_db
 from src.schemas import UserSchema, UserResponse, TokenModel
 from src.repository import users as repository_users
@@ -9,19 +12,22 @@ from src.services.auth import auth_service
 from src.services.email import send_email
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-import cloudinary
-import cloudinary.uploader
 
 router = APIRouter(prefix='/auth', tags=["auth"])
 limiter = Limiter(key_func=get_remote_address)
 
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def signup(body: UserSchema, db: Session = Depends(get_db)):
+async def signup(body: UserSchema, background_tasks: BackgroundTasks, request: Request, db: Session = Depends(get_db)):
     exist_user = await repository_users.get_user_by_email(body.email, db)
     if exist_user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
+    
     body.password = auth_service.get_password_hash(body.password)
     new_user = await repository_users.create_user(body, db)
+    
+    host = "http://localhost:8000" 
+    background_tasks.add_task(send_email, new_user.email, new_user.username, host)
+    
     return new_user
 
 @router.post("/login", response_model=TokenModel)
@@ -29,18 +35,18 @@ async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
     user = await repository_users.get_user_by_email(body.username, db)
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
+    
+    if not user.confirmed:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Email not confirmed")
+    
     if not auth_service.verify_password(body.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
     
     access_token = await auth_service.create_access_token(data={"sub": user.email})
     refresh_token = await auth_service.create_refresh_token(data={"sub": user.email})
     await repository_users.update_token(user, refresh_token, db)
+    
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
-
-@router.get("/me", response_model=UserResponse)
-@limiter.limit("5/minute")
-async def read_users_me(request: Request, current_user: repository_users.User = Depends(auth_service.get_current_user)):
-    return current_user
 
 @router.get("/confirmed_email/{token}")
 async def confirmed_email(token: str, db: Session = Depends(get_db)):
@@ -51,7 +57,12 @@ async def confirmed_email(token: str, db: Session = Depends(get_db)):
     if user.confirmed:
         return {"message": "Your email is already confirmed"}
     await repository_users.confirmed_email(email, db)
-    return {"message": "Email confirmed"}
+    return {"message": "Email confirmed successfully"}
+
+@router.get("/me", response_model=UserResponse)
+@limiter.limit("5/minute")
+async def read_users_me(request: Request, current_user: repository_users.User = Depends(auth_service.get_current_user)):
+    return current_user
 
 @router.patch('/avatar', response_model=UserResponse)
 async def update_avatar_user(file: UploadFile = File(), 
--- src/services/email.py
+++ src/services/email.py
@@ -2,12 +2,13 @@ import os
 from pathlib import Path
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 from src.services.auth import auth_service
+from pydantic import EmailStr
 
 conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_PORT=int(os.getenv("MAIL_PORT") or 465),
     MAIL_SERVER=os.getenv("MAIL_SERVER"),
     MAIL_FROM_NAME="Contacts App Service",
     MAIL_STARTTLS=False,
@@ -17,13 +18,17 @@ conf = ConnectionConfig(
     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 )
 
-async def send_email(email: str, username: str, host: str):
+async def send_email(email: EmailStr, username: str, host: str):
     try:
         token_verification = auth_service.create_email_token({"sub": email})
         message = MessageSchema(
             subject="Confirm your email",
             recipients=[email],
-            template_body={"host": host, "username": username, "token": token_verification},
+            template_body={
+                "host": host, 
+                "username": username, 
+                "token": token_verification
+            },
             subtype=MessageType.html
         )
         fm = FastMail(conf)
